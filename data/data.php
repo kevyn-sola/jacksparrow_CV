6ed20fb1dd72df9488f0d780e3cbd67c444a8ad1
toomanyMG <tchiki.tshiki@gmail.com>
2022-10-05 20:35:17 +0200

    fixed data.php

--- data/data.php
+++ data/data.php
@@ -103,36 +103,6 @@ $data = [
 
     ],
 
-    'experiencesProfessionnelles' => [
-
-        '1634 - 1640' => [
-            'Capitaine' => 'Capitaine du Black Pearl. Pillage & recherche de trésors en tout genre !'
-        ],
-        '1640' => [
-            'Capitaine' => 'Capitaine d\'un vieux raffiot avec une équipe de bras cassés. Objectif : Récupérer mon beau Black Pearl !'
-        ],
-        '1642 - 1644' => [
-            'Capitaine' => 'Capitaine du Black Pearl'
-        ],
-        '1644' => [
-            'Undefined' => 'Mort - RIP moi ! 🪦'
-        ],
-        '1646' => [
-            'Résurrection' => 'Plus mort !'
-        ],
-        '1649 - 1652' => [
-            'Capitaine' => 'Recherche de la Fontaine de Jouvence.'
-        ],
-
-    ],
-    'avis' => [
-
-        '../assets/img/legolas.jpg' => ['William Turner' => 'Cet homme est le meilleur arch... euh tireur que j\'ai rencontré de ma vie d\'elf... euh de pirate.'],
-        '../assets/img/barbossa.jpeg' => ['Hector Barbossa' => 'Je n\'ai jamais aussi bien vécu une mutinerie qu\'avec Jack Sparrow !'],
-        '../assets/img/elizabeth.jpg' => ['Elizabeth Swann' => 'Il ne s\'est jamais rien passé entre Jack et moi et il ne se passera jamais rien !!'],
-        '../assets/img/Gibbs.jpg' => ['Joshamee Gibbs' => 'Super Capitaine ! Quelle prestance ! Quel charisme ! J\'espère qu\'en disant ça il va enfin me payer tout ce qu\'il me doit.'],
-
-    ],
 
     'Loisirs' => [
         'Pecher' =>
