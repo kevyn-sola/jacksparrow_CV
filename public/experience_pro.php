db10ab45550234f53944811e3e8e26e03d9359c3
hhertout <hugues.hertout@gmail.com>
2022-10-05 16:37:30 +0200

    modify exp_pro

--- public/experience_pro.php
+++ public/experience_pro.php
@@ -36,8 +36,8 @@
                         <p><b><?= $name ?></b></p>
                     </div>
 
+                <?php endforeach ?>
             </div>
-        <?php endforeach ?>
     <?php endforeach ?>
     </div>
 </div>
