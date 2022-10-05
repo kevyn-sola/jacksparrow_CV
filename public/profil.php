4f78ba5117e03aa996773d39d956709b104fad78
toomanyMG <tchiki.tshiki@gmail.com>
2022-10-05 12:33:29 +0200

    Edited data.php

---
+++ public/profil.php
@@ -0,0 +1,37 @@
+<?php
+
+<div class="profil-jack">
+
+                <div class="img-icon-profil">
+                    <img src="/img/jack-profil.png" alt="Photo de Jack Sparrow">
+                    <div class="permis">
+                        <img src="/img/icon-anchor.png" alt="Icone d'une ancre de bateau" id="ancre-bateau">
+                        <p> Permis Bateau </p>
+                    </div>
+                </div>
+                <div class="content-profil">
+                    <h1>Capitaine Jack Sparrow</h1>
+                    <p>Seigneur pirate de la mer des Caraïbes, Née le 11 janvier 1693. <br>
+                        J’aime boire du rhum et séduire des femmes. <br>
+                        Toujours partant pour chercher quelque trésors surnaturel, rien de bon qu’une bonne quête à la
+                        recherche de richesse ! <br>
+                        Je suis particulièrement partisan du choix de la négociation plutôt que de faire parler les
+                        armes. <br>
+                        Certains vous diront que je suis un traitre mais ne faite pas attention à ce genre de calomnies.
+                        <br>
+
+                    </p>
+                    <p class="info-profil">
+                        Vous pouvez me retrouver à Nassau dans les Caraïbes <br>
+                        ou <br>
+                        Me contacter par perroquet voyageur de la compagnie Papagaio Livia en choisissant de remplir le
+                        formulaire à la fin de cette page. <br>
+
+
+
+                    </p>
+                </div>
+            </div>
+            <div class="sign_jack">
+                <img id="signature" src="/img/c2635b48197b847d0d05a39561d74aea.png" alt="signature jack" />
+            </div>
