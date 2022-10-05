b68645f5ec28071c39e5cbcdbd6bb9336f368533
hhertout <hugues.hertout@gmail.com>
2022-10-05 10:31:17 +0200

    modify folder structure

---
+++ data/data.php
@@ -0,0 +1,122 @@
+<?php
+
+$data = [
+    'profil' => [ 
+
+                'imgProfil' => ['/img/jack-profil.png'],
+                'altImgProfil' => ['Photo de Jack Sparrow'],
+                'iconProfil' => ['/img/icon-anchor.png'],
+                'altIconProfil' => ['Icone d\'une ancre de bateau'],
+                'permis' => ['Permis Bateau'],
+                'titreProfil' => ['Capitaine Jack Sparrow'],
+                'description' => ['Seigneur pirate de la mer des Caraïbes, Née le 11 janvier 1693. <br>
+                            J\’aime boire du rhum et séduire des femmes. <br>
+                            Toujours partant pour chercher quelque trésors surnaturel, rien de bon qu’une bonne quête à la
+                            recherche de richesse ! <br>
+                            Je suis particulièrement partisan du choix de la négociation plutôt que de faire parler les
+                            armes. <br>
+                            Certains vous diront que je suis un traitre mais ne faite pas attention à ce genre de calomnies.
+                            <br>'],
+                'contactProfil' => ['Vous pouvez me retrouver à Nassau dans les Caraïbes <br>
+                                ou <br>
+                                Me contacter par perroquet voyageur de la compagnie Papagaio Livia en choisissant de remplir le
+                                formulaire à la fin de cette page. <br>'],
+                'signProfil' => ['/img/c2635b48197b847d0d05a39561d74aea.png'], 
+                'altSignProfil' => ['signature jack'],
+
+        ],
+
+
+    'Competences' => [
+
+            'skillO1' =>[   
+                
+                'skillTitre1' => ['Bagarreur'],
+                'skilldesc1' => ['23 Batailles à mon actif !'],
+            ],
+
+            'skillO2' =>[
+
+                'skillTitre2' => ['Tiens l\'alcool'],
+                'skilldesc2' => ['Santé !'],
+            ],
+
+            'skillO3' =>[   
+                 
+                'skillTitre3' => ['Vol/Pillage'],
+                'skilldesc3' => ['Quel pirate n\'aime pas les trésors ?'],
+            ],
+
+            'skillO4' =>[ 
+
+                'skillTitre4' => ['Management'],
+                'skilldesc4' => ['Capitaine sur une dizaines de navires.'],
+            ],
+
+            'skillO5' =>[ 
+
+                'skillTitre5' => ['Rapide'],
+                'skilldesc5' => ['Je cours plus vite qu\'une tribu de cannibales.'],
+            ],
+                    ],   
+                    
+    
+    'experiencesProfessionnelles' => [
+
+                        '1634 - 1640' => [
+                             'Capitaine' => 'Capitaine du Black Pearl. Pillage & recherche de trésors en tout genre !'
+                         ],
+                         '1640' => [
+                             'Capitaine' => 'Capitaine d\'un vieux raffiot avec une équipe de bras cassés. Objectif : Récupérer mon beau Black Pearl !'
+                         ],
+                         '1642 - 1644' => [
+                             'Capitaine' => 'Capitaine du Black Pearl'
+                         ],
+                         '1644' => [
+                             'Undefined' => 'Mort - RIP moi ! 🪦'
+                         ],
+                         '1646' => [
+                             'Résurrection' => 'Plus mort !'
+                         ],
+                         '1649 - 1652' => [
+                             'Capitaine' => 'Recherche de la Fontaine de Jouvence.'
+                         ],
+                 
+                        ],
+    'avis' => [
+                 
+                         'img/legolas.jpg' => ['William Turner' => 'Cet homme est le meilleur arch... euh tireur que j\'ai rencontré de ma vie d\'elf... euh de pirate.'],
+                         'img/barbossa.jpeg' => ['Hector Barbossa' => 'Je n\'ai jamais aussi bien vécu une mutinerie qu\'avec Jack Sparrow !'],
+                         'img/elizabeth.jpg' => ['Elizabeth Swann' => 'Il ne s\'est jamais rien passé entre Jack et moi et il ne se passera jamais rien !!'],
+                         'img/Gibbs.jpg' => ['Joshamee Gibbs' => 'Super Capitaine ! Quelle prestance ! Quel charisme ! J\'espère qu\'en disant ça il va enfin me payer tout ce qu\'il me doit.'],
+                 
+                     ],
+
+    'Loisirs' => [
+
+            "hobby01" => [  
+            
+                'hobbyTitle1' => ['Pecher'],
+                'hobbyDescr1' => ['Kraken, merle, sardine.'],
+            ],
+
+            "hobby02" => [  
+
+                'hobbyTitle2' => ['Escrime'],
+                'hobbyDescr2' => ['J\'aime la bagarre.'],
+            ],
+
+            "hobby03" => [  
+
+                'hobbyTitle3' => ['Tir sportif'],
+                'hobbyDescr3' => ['Pan, pan, pan.'],
+            ],
+
+            "hobby04" => [ 
+
+                'hobbyTitle4' => ['Rhum'],
+                'hobbyDescr4' => ['Juste le boire, pas le distiller.'],
+            ],
+
+    ],
+];
