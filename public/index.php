8927a2e57859a4a6e147479305ac84e922eaf682
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-06 22:08:36 +0200

    remove empty img + com

--- public/index.php
+++ public/index.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="android-chrome-icon" sizes="192x192" href="../assets/img/favicon_io/android-chrome-192x192.png">
+    <link rel="android-chrome-icon" sizes="512x512" href="../assets/img/favicon_io/android-chrome-512x512.png">
+    <link rel="apple-touch-icon" href="../assets/img/favicon_io/apple-touch-icon.png">
+    <link rel="icon" sizes="16x16" href="../assets/img/favicon_io/favicon-16x16.png">
+    <link rel="icon" sizes="32x32" href="../assets/img/favicon_io/favicon-32x32.png">
+    <link rel="icon" href="../assets/img/favicon_io/favicon.ico">
+    <link rel="manifest" href="../assets/img/favicon_io/site.webmanifest">
     <link rel="stylesheet" href="../assets/style/root.css" />
     <link rel="stylesheet" href="../assets/style/style.css" />
     <link rel="stylesheet" href="../assets/style/form.css" />
